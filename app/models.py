0efeda986ca6934d9a542518c8841e6d8943c0c0
enock24 <enookimuu@gmail.com>
2020-12-06 11:29:41 +0300

    add migration import to manage.py

--- app/models.py
+++ app/models.py
@@ -4,8 +4,11 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
         return f'User {self.username}'
@@ -15,6 +18,8 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
 
     def __repr__(self):
         return f'User {self.name}'
--- manage .py
+++ manage .py
@@ -1,9 +1,10 @@
 from app import create_app,db
 # .....
-from app.models import User
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Role = Role )
 if __name__ == '__main__':
     manager.run()
