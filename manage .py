55954095f172a0f4f553aa173095e58c3629a1f8
enock24 <enookimuu@gmail.com>
2020-12-06 11:40:59 +0300

    add unittest

--- manage .py
+++ manage .py
@@ -3,6 +3,12 @@ from app import create_app,db
 from app.models import User,Role
 from  flask_migrate import Migrate, MigrateCommand
 
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User, Role = Role )
